5d1b0ece3a1bb7c2f443ae795cd3c332f4f304c9
twalt007 <tatumwalter@gmail.com>
2020-03-09 21:09:20 -0700

    woring on resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,9 +6,15 @@ class Resume extends Component {
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        if(education.description){
+          return <div key={education.school}><h3>{education.school}</h3>
+          <p className="info">{education.degree}</p>
+          <p>{education.description}</p></div>
+        } 
+        else {
+            return <div key={education.school}><h3>{education.school}</h3>
+            <p className="info">{education.degree}</p></div>
+        }
       })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
