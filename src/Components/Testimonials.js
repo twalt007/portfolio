2751535df8e65290e685fa0acb7f7f88fd6a7365
twalt007 <tatumwalter@gmail.com>
2020-05-23 08:21:59 -0700

    all minor touch-ups fixed

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -18,18 +18,20 @@ class Testimonials extends Component {
     return (
       <section id="testimonials">
       <div className="text-container">
-         <div className="row">
+         <div className="focus">
+            <div className="row">
 
-            <div className="two columns header-col">
-               <h1><span>Client Testimonials</span></h1>
-            </div>
-
-            <div className="ten columns flex-container">
-                  <ul className="slides">
-                      {testimonials}
-                  </ul>
+               <div className="two columns header-col">
+                  <h1><span>Client Testimonials</span></h1>
                </div>
-            </div>
+
+               <div className="ten columns flex-container">
+                     <ul className="slides">
+                        {testimonials}
+                     </ul>
+                  </div>
+               </div>
+            </div>   
          </div>
    </section>
     );
